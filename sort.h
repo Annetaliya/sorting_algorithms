43f7a028414a806eba66d8c3f7268fe621dd6c9b
Annette Adhiambo <annetaliya@gmail.com>
2023-01-24 02:09:37 -0800

    quick sort

--- sort.h
+++ sort.h
@@ -21,6 +21,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
+void quick_sort(int *array, size_t size);
 
 
 #endif
